1b423b11f34d888dfd9494c07393bd239c9073d3
ChrissJones <94918921+ChrissJones@users.noreply.github.com>
2022-01-18 17:35:04 -0500

    push

--- Easy/easy.js
+++ Easy/easy.js
@@ -1,3 +1,4 @@
-var  student = [`Chris Jones`, `Ivanna Contreras`, `Yasmine Jimenez`]
-var age = [`23`, `24`, `22`]
+var  student = [`Ivanna Contreras`, `Chris Jones`, `Yasmine Jimenez`]
+var age = [`24`, `23`, `22`]
 console.log("Hello, My name is " + student[1] + ` and I am ` + age[1]+ ` years old.`)
+/*Worked with Ivanna for this Challenge*/
